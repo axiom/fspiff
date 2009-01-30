6a531cb3b220c97c952b720537f084d44a54652b
Johannes Martinsson <johannes@antiklimax.se>
2009-01-30 17:04:26 +0100

    imported current version of files2xspf

---
+++ files2xspf.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+require "rubygems"
+require "taglib"
+require "builder"
+
+BASE="/srv/music/"
+
+xspf = Builder::XmlMarkup.new(:target => $stdout, :indent => 1)
+
+xspf.instruct!
+
+xspf.playlist do
+
+	xspf.title ""
+	xspf.creator ""
+	xspf.info ""
+
+	xspf.tracklist do
+
+		ARGF.each do |fn|
+			begin
+				f = TagLib::File.new(BASE + fn.strip)
+
+				xspf.track do
+					xspf.title f.title if f.title
+					xspf.creator f.artist if f.artist
+					xspf.album f.album if f.album
+					xspf.duration f.length * 1000 if f.length
+					xspf.trackNum f.track if f.track
+				end
+			rescue Mahoro::Error
+			rescue TagLib::BadFile
+			rescue TagLib::BadTag
+			ensure
+				f.close unless f.nil?
+			end
+		end
+
+	end
+end
